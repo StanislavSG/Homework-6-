6b354974461099b84603b3d9c5880bd9b5163c5e
Stanislav <spotes@yandex.ru>
2024-04-03 14:31:28 +0300

    Task 3

---
+++ Task1/Task2/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Андрей";
+        bool isPalindrome = IsPalindrome(input);
+        Console.WriteLine($"Строка '{input}' является палиндромом: {isPalindrome}");
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        if (str.Length % 2 != 0) return false; // Проверка на четность длины строки
+
+        for (int i = 0; i < str.Length / 2; i++)
+        {
+            // Проверка на равенство символов без учета регистра
+            if (char.ToLower(str[i]) != char.ToLower(str[str.Length - 1 - i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
