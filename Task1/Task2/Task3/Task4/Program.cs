4aa1eb097bde2dac6d994648d2b941e7bf1b1239
Stanislav <spotes@yandex.ru>
2024-04-03 14:34:45 +0300

    Task 3

---
+++ Task1/Task2/Task3/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. Сформировать строку, в которой слова расположены в обратном порядке.
+// В полученной строке слова должны быть также разделены пробелами.
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string originalString = "Hello World This Is A Test String";
+        string reversedString = ReverseString(originalString);
+        Console.WriteLine(reversedString);
+    }
+
+    static string ReverseString(string input)
+    {
+        // Разделяем строку на слова
+        string[] words = input.Split(' ');
+
+        // Переворачиваем список слов
+        Array.Reverse(words);
+
+        // Собираем новую строку, разделяя слова пробелами
+        return string.Join(" ", words);
+    }
+}
+
