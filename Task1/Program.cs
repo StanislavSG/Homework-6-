bb9b4d71bd6c37a4005956347cc165a4e351be9c
Stanislav <spotes@yandex.ru>
2024-04-03 14:14:06 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Размеры массива
+        const int rows = 3;
+        const int cols = 4;
+
+        // Создание двумерного массива символов
+        char[,] matrix = new char[rows, cols];
+
+        // Заполнение массива символами
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                matrix[i, j] = (char)('a' + i  *  cols + j); // Пример заполнения
+
+        // Создание строки из символов массива
+        string resultString = "";
+        foreach (var item in matrix)
+            resultString += item;
+
+        Console.WriteLine(resultString);
+    }
+}
+
